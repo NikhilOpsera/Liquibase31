ef2d92660792c43b3b3ba77f58dab761f177b353
NikhilOpsera <nikhil@opsera.io>
2023-12-17 11:58:37 +0530

    fourth

---
+++ schema3.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Trail123 ( PersonID int, City varchar (255) );
+insert into Trail VALUES (1,'Bengaluru');
+insert into Trail VALUES (2,'Belgavi');
+insert into Trail VALUES (3,'Bellur');
