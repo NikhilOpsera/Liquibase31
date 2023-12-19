a4382850dfdf919c4ead2d01884e1e92fdaedb3d
NikhilOpsera <137175449+NikhilOpsera@users.noreply.github.com>
2023-12-19 16:49:52 +0530

    Update schema5.sql

--- schema5.sql
+++ schema5.sql
@@ -1,1 +1,4 @@
-insert into Trail345 VALUES (4,'Bihar');
+CREATE TABLE Trailedw_test ( PersonID int, City varchar (255) );
+insert into Trailedw_test VALUES (1,'Bengaluru');
+insert into Trailedw_test VALUES (2,'Belgavi');
+insert into Trailedw_test VALUES (3,'Bellur');
