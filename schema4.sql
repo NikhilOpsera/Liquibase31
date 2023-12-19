99acc5243ee0aeb5b50fd1cb489b62369d869cb6
NikhilOpsera <137175449+NikhilOpsera@users.noreply.github.com>
2023-12-19 10:59:18 +0530

    Update schema4.sql

--- schema4.sql
+++ schema4.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Trail345 ( PersonID int, City varchar (255) );
-insert into Trail345 VALUES (1,'Bengaluru');
-insert into Trail345 VALUES (2,'Belgavi');
-insert into Trail345 VALUES (3,'Bellur');
+CREATE TABLE Trail645 ( PersonID int, City varchar (255) );
+insert into Trail645 VALUES (1,'Bengaluru');
+insert into Trail645 VALUES (2,'Belgavi');
+insert into Trail645 VALUES (3,'Bellur');
